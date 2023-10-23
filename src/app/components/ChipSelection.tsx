7887ed6157c9d92cacfa4ae7e5d814fb3de01636
TahreemKD <tahreem.iqbal@khareed.pk>
2023-10-23 14:33:31 +0400

    TS fixes

--- src/app/components/ChipSelection.tsx
+++ src/app/components/ChipSelection.tsx
@@ -1,34 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState, MouseEvent } from 'react';
 import Chip from '@mui/material/Chip';
-import { useEffect, useState } from 'react';
 
 const chips = ['Apple', 'Meta', 'Netflix', 'Google', 'Twitter', 'Tesla'];
 
-const ChipSelection = ({onChipSelection,}) => {
-  const [selectedChip, setSelectedChip] = useState('');
+interface ChipSelectionProps {
+  onChipSelection: (selectedChip: string) => void;
+}
+
+const ChipSelection: React.FC<ChipSelectionProps> = ({ onChipSelection }) => {
+  const [selectedChip, setSelectedChip] = useState<string>('');
   const defaultChip = chips[0];
 
-  const handleChipClick = (event: React.MouseEvent<HTMLDivElement>, chipValue: string) => {
+  const handleChipClick = (event: MouseEvent<HTMLDivElement>, chipValue: string) => {
     setSelectedChip(chipValue);
   };
 
   useEffect(() => {
-   onChipSelection(selectedChip);
+    onChipSelection(selectedChip);
   }, [selectedChip]);
 
   return (
-    <div className='w-full text-center mt-5'>
-        {chips.map((chipValue, index) => {
-            return (
-                <Chip
-                    sx={{marginLeft: "10px", marginTop: "5px"}}
-                    key={index}
-                    label={chipValue}
-                    onClick={(event) => handleChipClick(event, chipValue)}
-                    classes={{root: selectedChip == chipValue ||  selectedChip == "" && chipValue ==  defaultChip? 'selected-chip': ''}}
-                />
-            );
-        })}
+    <div className="w-full text-center mt-5">
+      {chips.map((chipValue, index) => (
+        <Chip
+          sx={{ marginLeft: '10px', marginTop: '5px' }}
+          key={index}
+          label={chipValue}
+          onClick={(event) => handleChipClick(event, chipValue)}
+          classes={{
+            root:
+              selectedChip === chipValue || (selectedChip === '' && chipValue === defaultChip)
+                ? 'selected-chip'
+                : '',
+          }}
+        />
+      ))}
     </div>
   );
 };
